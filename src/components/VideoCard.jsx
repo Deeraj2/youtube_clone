b97216ef5fd5a5ef3900c3ae443b59c79e3e22d6
Deeraj <deerajrajesh22@gmail.com>
2023-01-17 17:14:49 +0530

    Fixed scrollbar and video ui

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,34 +1,76 @@
-import { Link } from 'react-router-dom';
-import { Typography, Card, CardContent, CardMedia} from '@mui/material';
-import { CheckCircle } from '@mui/icons-material';
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
-import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
 
-const VideoCard = ({ video: { id: {videoId }, snippet } }) => {
-
-
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
   return (
-    <Card sx={{ width: {md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
-      <Link to={videoId ? `/video/${videoId}` : `/video/${videoId}` }>
-        <CardMedia image={snippet?.thumbnails?.high.url || demoThumbnailUrl} alt={snippet?.title} 
-        sx={{ width: { xs: '100%', sm: '358px'}, height: 180 }} 
-         />
+    <Card
+      sx={{
+        width: { md: "320px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: 0,
+        borderRadius: "6px",
+        backgroundColor: "transparent",
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : `/video/${videoId}`}>
+        <CardMedia
+          image={snippet?.thumbnails?.high.url || demoThumbnailUrl}
+          alt={snippet?.title}
+          sx={{
+            width: { xs: "100%", sm: "358px" },
+            height: 180,
+            borderTopLeftRadius: "5px",
+            borderTopRightRadius: "5px",
+            objectFit: "cover",
+          }}
+        />
       </Link>
-      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px'}}>
+      <CardContent
+        sx={{
+          backgroundColor: "#1e1e1e",
+          height: "106px",
+          borderBottomLeftRadius: "5px",
+          borderBottomRightRadius: "5px",
+        }}
+      >
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-          <Typography variant="subtitle1">
+          <Typography variant="subtitle1" color="#fff">
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
           </Typography>
         </Link>
-        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
-        <Typography variant="subtitle2" color="gray">
-          {snippet?.channelTitle || demoChannelTitle}
-          <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
-        </Typography>
-      </Link>
-      </CardContent> 
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography
+            variant="subtitle2"
+            color="gray"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
